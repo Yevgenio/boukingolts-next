30e6d5352ca138d08176cfe37f27e32785e04f2b
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2025-05-08 04:04:28 +0300

    image issues fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'boukingolts.art', // OR domain name if you have one
+        port: '5000',                   // Backend port if not 80
+        pathname: '/api/uploads/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
