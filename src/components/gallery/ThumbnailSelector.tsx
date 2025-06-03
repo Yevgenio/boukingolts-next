1372274d50a0762ce9b7484f29da01c2d3ba3973
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2025-06-03 20:59:16 +0300

    ThumbnailSelector.tsx: Update thumbnail selector to handle new image formats

--- src/components/gallery/ThumbnailSelector.tsx
+++ src/components/gallery/ThumbnailSelector.tsx
@@ -2,6 +2,7 @@
 
 import API_URL from '@/config/config';
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
 // import Image from 'next/image';
 
 interface ImageType {
@@ -32,9 +33,10 @@ export default function ThumbnailSelector({ product }: { product: Product }) {
       <div className="flex gap-2 overflow-x-auto">
         {product.images.map((img, index) => (
           <button key={img._id} onClick={() => setActiveIndex(index)}>
-            <img
+            <Image
               src={`${API_URL}/api/uploads/${img.url}`}
               alt={`Thumbnail ${index + 1}`}
+              fill
               className={`w-20 h-20 ${index === activeIndex ? 'ring-2 ring-black' : ''}`}
             />
           </button>
