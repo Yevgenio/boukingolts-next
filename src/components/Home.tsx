3463c45ad0d66b77b2f0bc474c3384ed34de5498
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2025-06-06 01:07:54 +0300

    new grid homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,12 @@
 // pages/home.tsx
 
+import ColorGrid from '@/components/grid/ColorGrid';
+
 const Home = () => {
   return (
-    <section className="p-8 text-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to my art portfolio.</h1>
-        <p className="text-lg text-gray-600">
-          Explore the beauty and creativity captured through art. Dive into the gallery to find inspiration.
-        </p>
-      </section>
+    <section className="w-full h-full">
+      <ColorGrid />
+    </section>
   );
 };
 
