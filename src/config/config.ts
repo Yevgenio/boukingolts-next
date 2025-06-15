6b982800505e10f87b9126db0c359e3340d5bd9e
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2025-06-16 01:52:38 +0300

    api url fix 2

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
-const API_URL = "https://boukingolts.art";
+// const API_URL = "https://boukingolts.art";
 // const API_URL = "http://localhost:5000";
-// const API_URL = "";
+const API_URL = "";
 
 export default API_URL;
