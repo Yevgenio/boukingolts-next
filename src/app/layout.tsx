12c03e9a5d7248f38e868b06a606a59008792301
Yevgeni Boukingolts <Yevgeni.b93@gmail.com>
2025-06-17 07:24:07 +0300

    changed favicon and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,10 @@ import { AuthProvider } from '@/context/AuthContext';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>Boukingolts</title>
+        {/* <link rel="icon" type="image/x-icon" href="/favicon.ico" /> */}
+      </head>
       <body>
         
         <AuthProvider>
